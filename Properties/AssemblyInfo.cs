9e21aab2ef0b841433d1531401a6b839aeebd541
Igor Z <zhukovsky.igor@me.com>
2012-10-25 12:08:20 +0300

    add assembly info

---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,24 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MobyDick Testkit")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MobyDick.TestKit.Properties")]
+[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+[assembly: System.Reflection.AssemblyVersion("1.1")]
+[assembly: System.Reflection.AssemblyFileVersion("1.1.1.4224")]
+[assembly: System.Reflection.AssemblyInformationalVersion("1.1.1 rev. 121025")]
